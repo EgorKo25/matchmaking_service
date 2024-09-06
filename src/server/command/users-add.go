457a42426240b0430ee38f73b2459d58004b0589
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-06 12:46:01 +0300

    добавлена валидация полей

--- src/server/command/users-add.go
+++ src/server/command/users-add.go
@@ -2,17 +2,19 @@ package command
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"matchamking/core"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type UserAdd struct {
-	PlayerName string  `json:"name"`
-	Latency    float64 `json:"latency"`
-	Skill      float64 `json:"skill"`
+	PlayerName string  `json:"name" validate:"required,min=2,max=100"`
+	Latency    float64 `json:"latency" validate:"required,gte=0"`
+	Skill      float64 `json:"skill" validate:"required,gte=0"`
 	CreatedAt  time.Time
 }
 
@@ -28,6 +30,14 @@ func (u *UserAdd) Parse(ctx *gin.Context) error {
 		return err
 	}
 	u.CreatedAt = time.Now()
+	validate := validator.New()
+	err := validate.Struct(u)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			return fmt.Errorf("Field: %s, Error: %s\n", err.Field(), err.Tag())
+		}
+		return err
+	}
 	return nil
 }
 
