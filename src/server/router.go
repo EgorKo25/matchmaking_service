683067fcdb4d473847d907f367c98af04700313e
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-06 11:09:33 +0300

    реализован роутер

---
+++ src/server/router.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"matchamking/server/command"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getRouter(commands command.Commander) *gin.Engine {
+	router := gin.Default()
+	v1 := router.Group("/v1")
+	var c command.ICommand
+	c = &command.UserAdd{}
+	v1.POST("/"+c.Name(), commands.Register(c))
+	return router
+}
