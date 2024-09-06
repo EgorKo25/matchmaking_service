bd283564559ebd97553b41d8803e34d480827a28
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-06 16:24:51 +0300

    gosimple

--- src/server/router.go
+++ src/server/router.go
@@ -9,8 +9,7 @@ import (
 func getRouter(commands command.Commander) *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
-	var c command.ICommand
-	c = &command.UserAdd{}
+	c := &command.UserAdd{}
 	v1.POST("/"+c.Name(), commands.Register(c))
 	return router
 }
