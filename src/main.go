c1319a6d0ef71a08d52ccc9c5f3040145d1a4bfd
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-06 12:46:38 +0300

    написан прототип main

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	l "log"
+
+	"matchamking/config"
+	"matchamking/logger"
+	"matchamking/server"
+	"matchamking/server/command"
+)
+
+func main() {
+	log, err := logger.NewLogger(logger.PRODUCTION)
+	if err != nil {
+		l.Fatalf("cannot create logger: %s", err.Error())
+	}
+	cfg, err := config.NewMSConfig()
+	if err != nil {
+		log.Fatal("cannot load configuration with error: %s", err.Error())
+	}
+	manager := command.NewManager(log)
+
+	s := server.NewServer(cfg.ServerConfig, manager, log)
+	if err = s.Run(context.Background()); err != nil {
+		log.Fatal("%s", err.Error())
+	}
+}
