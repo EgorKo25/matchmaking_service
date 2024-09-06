84c02d97cebe51af9998f3946eefd2fe83867b04
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-06 20:15:04 +0300

    создана абстракция над хранилищем

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 	"matchamking/src/logger"
 	"matchamking/src/server"
 	"matchamking/src/server/command"
+	"matchamking/src/storage"
 )
 
 func main() {
+	ctx := context.Background()
 	log, err := logger.NewLogger(logger.PRODUCTION)
 	if err != nil {
 		l.Fatalf("cannot create logger: %s", err.Error())
@@ -20,10 +22,13 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot load configuration with error: %s", err.Error())
 	}
-	manager := command.NewManager(log)
+	if err = storage.InitStorage(ctx, cfg.Storage); err != nil {
+		log.Fatal("cannot create storage with error: %s", err.Error())
+	}
 	core.InitMatchmaker(cfg.MatchmakerConfig)
+	manager := command.NewManager(log)
 	s := server.NewServer(cfg.ServerConfig, manager, log)
-	if err = s.Run(context.Background()); err != nil {
+	if err = s.Run(ctx); err != nil {
 		log.Fatal("%s", err.Error())
 	}
 }
