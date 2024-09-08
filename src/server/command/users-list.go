791408eef37f24d545a20ca8a4ecf9d45f9a0412
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-08 23:22:04 +0300

    добавлен новый обработчик для вывода списка пользователей

---
+++ src/server/command/users-list.go
@@ -0,0 +1,48 @@
+package command
+
+import (
+	"matchamking/src/core"
+	"matchamking/src/storage"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UsersList struct {
+	users []*Player
+}
+
+type Player struct {
+	PlayerName string  `json:"name" validate:"required,min=2,max=100"`
+	Latency    float64 `json:"latency" validate:"required,gte=0"`
+	Skill      float64 `json:"skill" validate:"required,gte=0"`
+}
+
+func (u *UsersList) Name() string {
+	return "users/list"
+}
+
+func (u *UsersList) Parse(_ *gin.Context) error {
+	return nil
+}
+
+func (u *UsersList) Apply(ctx *gin.Context) (any, error) {
+	mmStorage := storage.GetStorage()
+	players, err := mmStorage.GetAllPlayers(ctx)
+	if err != nil {
+		return nil, err
+	}
+	u.users = custToListPlayers(players)
+	return u, err
+}
+
+func custToListPlayers(players []*core.Player) []*Player {
+	result := make([]*Player, len(players))
+	for _, v := range players {
+		result = append(result, &Player{
+			PlayerName: v.Name,
+			Latency:    v.Latency,
+			Skill:      v.Skill,
+		})
+	}
+	return result
+}
--- src/server/router.go
+++ src/server/router.go
@@ -9,7 +9,10 @@ import (
 func getRouter(commands command.Commander) *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
-	c := &command.UserAdd{}
+	var c command.ICommand
+	c = &command.UserAdd{}
+	v1.POST("/"+c.Name(), commands.Register(c))
+	c = &command.UsersList{}
 	v1.POST("/"+c.Name(), commands.Register(c))
 	return router
 }
