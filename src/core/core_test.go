cadd6b6baf4b9696809ef00f1855d6781e4903d2
kozlov-e <kozlov-e@protei-lab.ru>
2024-09-08 22:46:01 +0300

    расширены тесты ядра

--- src/core/core_test.go
+++ src/core/core_test.go
@@ -7,12 +7,89 @@ import (
 	"testing"
 	"time"
 
+	"matchamking/src/config"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestCheckApproximatelyEqual(t *testing.T) {
 	require.Truef(t, checkApproximatelyEqual(100, 105, 5), "expected values to be approximately equal")
 	require.Falsef(t, checkApproximatelyEqual(100, 110, 5), "expected values to not be approximately equal")
+}
+
+func TestInitMatchmaker(t *testing.T) {
+	// проверяем, что объект будет вызван лишь единожды
+	InitMatchmaker(&config.MatchmakerConfig{AcceptableWaitingTime: 15})
+	core := GetMatchmakingCore()
+	InitMatchmaker(&config.MatchmakerConfig{AcceptableWaitingTime: 25})
+	core1 := GetMatchmakingCore()
+	require.Equal(t, core, core1)
+}
+
+func setupTestMatchmakingCore() *MatchmakingCore {
+	InitMatchmaker(
+		&config.MatchmakerConfig{AcceptableWaitingTime: 15, DeltaSkill: 10, DeltaLatency: 50, GroupSize: 3})
+	return GetMatchmakingCore()
+}
+
+func TestFindGroup_CreateNewGroup(t *testing.T) {
+	mmCore := setupTestMatchmakingCore()
+	player := randomPlayer()
+
+	mmCore.FindGroup(player)
+	require.Equal(t, 1, len(mmCore.groups))
+	require.Equal(t, player, mmCore.groups[0].players[0])
+	require.Equal(t, int32(1), mmCore.groups[0].totalPlayers)
+}
+
+func TestFindGroup_BoundaryConditions(t *testing.T) {
+	mmCore := setupTestMatchmakingCore()
+
+	group := &Group{
+		players:                   []*Player{},
+		averagePermissibleSkill:   50.0,
+		averagePermissibleLatency: 100.0,
+		differenceSkill:           10.0,
+		differenceLatency:         50.0,
+		totalPlayers:              0,
+	}
+	mmCore.groups = append(mmCore.groups, group)
+
+	player := &Player{
+		Name:    "Player4",
+		Skill:   60.0,  // Навык ровно на границе
+		Latency: 150.0, // Латентность ровно на границе
+	}
+
+	mmCore.FindGroup(player)
+
+	require.Equal(t, 1, len(mmCore.groups))
+	require.Equal(t, player, mmCore.groups[0].players[0])
+}
+
+func TestFindGroup_FillGroupAndRemove(t *testing.T) {
+	mmCore := setupTestMatchmakingCore()
+
+	group := &Group{
+		players:                   []*Player{},
+		averagePermissibleSkill:   50.0,
+		averagePermissibleLatency: 100.0,
+		differenceSkill:           10.0,
+		differenceLatency:         50.0,
+		totalPlayers:              0,
+	}
+	mmCore.groups = append(mmCore.groups, group)
+
+	players := [3]*Player{
+		{Name: "Player1", Skill: 55.0, Latency: 110.0},
+		{Name: "Player2", Skill: 52.0, Latency: 105.0},
+		{Name: "Player3", Skill: 53.0, Latency: 108.0},
+	}
+
+	for _, player := range players {
+		mmCore.FindGroup(player)
+	}
+	require.Equal(t, 0, len(mmCore.groups))
 }
 
 // Генерация случайного игрока
